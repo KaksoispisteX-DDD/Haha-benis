0cc22a28225546fc6e7ff9db6c710434f2e41b3a
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-10 16:53:26 +0200

    import until.Scanner a funkcia Fajinovica

--- Kaksoispiste/src/kaksoispiste/Kaksoispiste.java
+++ Kaksoispiste/src/kaksoispiste/Kaksoispiste.java
@@ -1,9 +1,18 @@
 
 package kaksoispiste;
+import java.util.Scanner;
+
 
 public class Kaksoispiste {
 
-    
+public void Fajinovica() {
+
+
+
+
+
+
+}    
 
      
     public static void main(String[] args) {
