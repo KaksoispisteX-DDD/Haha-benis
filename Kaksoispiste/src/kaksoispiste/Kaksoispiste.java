9095247a71370e23af6920346592a6d3cdec3060
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-10 16:46:36 +0200

    zmazane komentare

--- Kaksoispiste/src/kaksoispiste/Kaksoispiste.java
+++ Kaksoispiste/src/kaksoispiste/Kaksoispiste.java
@@ -10,9 +10,9 @@ package kaksoispiste;
  */
 public class Kaksoispiste {
 
-    /**
-     * @param args the command line arguments
-     */
+    
+
+     
     public static void main(String[] args) {
         // TODO code application logic here
     }
