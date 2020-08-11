e924a69b270a0a825cc0ae42de0a1c78af1da18c
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-11 12:00:56 +0200

    pridanie scannera

--- Faktorial/src/App.java
+++ Faktorial/src/App.java
@@ -1,4 +1,17 @@
+
+
+import java.util.Scanner;
+
 public class App {
+    Scanner novy = new Scanner(System.in);
+    int cislo = 0;
+    long vysledok = 1;
+    
+
+
+
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
