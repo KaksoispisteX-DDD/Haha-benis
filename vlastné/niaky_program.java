d65481abd58406e010d7bc7be476cfc1d13f87c9
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-29 19:02:41 +0200

    pridanie funkcie pyramida

--- vlastné/niaky_program.java
+++ vlastné/niaky_program.java
@@ -6,12 +6,22 @@ import java.util.Scanner;
  */
 public class niaky_program {
     Scanner vstup = new Scanner(System.in);
-    
+    public void pyramida(){
 
     
+    if (vstup.equals("pyramida")){
+        System.out.println("Pyramidka");
+
+
+    }
+    else {
+        System.out.println("Si kokot");
+
+    }
 
 
 
+    }
     public static void main(String[] args) {
         
         
