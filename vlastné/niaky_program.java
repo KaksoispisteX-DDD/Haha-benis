6eb6b976372b4447b9895f61cbc1996b380c1bc1
AdvencedmanSK <patrik.sramek0@gmail.com>
2020-08-29 18:52:20 +0200

    Pridanie scannera

--- vlastné/niaky_program.java
+++ vlastné/niaky_program.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
+
 /**
  * niaky_program
  */
 public class niaky_program {
+    Scanner vstup = new Scanner(System.in);
+    switch (vstup) {
+        case "pyramida":
+        System.out.println("Bruh");
+            
+            break;
+    
+        default:
+            break;
+    }
+
+
 
     public static void main(String[] args) {
         
     }
+
 }
